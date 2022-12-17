0d40f1691e5dcd6c9e2c2bc39ce8e9c08b3b825c
yulduzoromova <yulduzoromova1993@gmail.com>
2022-12-17 12:44:24 +0300

    Tsikl amaliyot

---
+++ 14-dars/script.js
@@ -0,0 +1,18 @@
+let num = 1;
+
+//first way
+//  while (num <=10) {
+//     console.log(num);
+//     num++;
+//  }
+
+// second
+// do {
+//     console.log(num);
+//     num++;
+// }while (num <= 10)
+
+//third
+for (let i = 1; i <= 9; i++) {
+    console.log(i)
+}
